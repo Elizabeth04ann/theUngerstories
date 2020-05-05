82a15d3714e7016166f92e1b573cb1e570a310b6
Elizabeth04ann <54286041+Elizabeth04ann@users.noreply.github.com>
2020-05-05 07:38:41 -0400

    first upload

---
+++ The Unger Stories/main.js
@@ -0,0 +1,9 @@
+function navBarresponsive() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
+  
